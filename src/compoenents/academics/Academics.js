a70bc7222f86ef17293a5c66085eeb20e555440f
Alok Yadav <alokyadav9821238248@gmail.com>
2021-08-16 13:55:13 +0530

    changed academics page

--- src/compoenents/academics/Academics.js
+++ src/compoenents/academics/Academics.js
@@ -9,9 +9,9 @@ const Academics = () => {
     return (
         <>
         <div className="Academics">
-           <div className="widecard">
+           {/* <div className="widecard">
            <img src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="mountains pic"/>
-          </div>
+          </div> */}
           <div>
             <h1>Creating a Brighter Tommorrow </h1><br />
            <p>small sentence </p><br />
@@ -21,21 +21,77 @@ const Academics = () => {
             </div>
          
         </div>
-        <div className="cardholder">
+        {/* <div className="cardholder">
           <Timeline/>
-        </div>
-        <div className="feature">
-            <Features/>
-        </div>
-        <div className="card3holder">
-            <Card3/>
-            <Card3/>
-            <Card3/>
-        </div>
-        <div className="infoholder">
-            <Info/>
-        </div>
-       
+        </div> */}
+      
+      <div className="programnames">
+          <div className="programcard">
+              <h3>PROGRAM 1</h3>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam culpa minus vel unde aperiam amet adipisci eligendi, recusandae dolore reprehenderit  </p>
+          </div>
+          <div className="programcard">
+          <h3>PROGRAM 2</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam culpa minus vel unde aperiam amet adipisci eligendi, recusandae dolore reprehenderit  </p>
+          </div>
+          <div className="programcard">
+          <h3>PROGRAM 3</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam culpa minus vel unde aperiam amet adipisci eligendi, recusandae dolore reprehenderit  </p>
+          </div>
+          <div className="programcard">
+          <h3>PROGRAM 4</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam culpa minus vel unde aperiam amet adipisci eligendi, recusandae dolore reprehenderit  </p>
+          </div>
+          <div className="programcard">
+          <h3>PROGRAM 5</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam culpa minus vel unde aperiam amet adipisci eligendi, recusandae dolore reprehenderit  </p>
+          </div>
+          <div className="programcard">
+          <h3>PROGRAM 6</h3>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam culpa minus vel unde aperiam amet adipisci eligendi, recusandae dolore reprehenderit  </p>
+          </div>
+      </div>
+     
+     
+      <div className="programdetails">
+          <div className="firstprogram">
+              <div className="leftside">
+                  
+              </div>
+              <div className="rightside">
+                  
+                  <h5>case study</h5>
+                  <h2>Resiliency</h2> <br />
+                  <p>The Northeast Resiliency Consortium worked with Smart Sparrow to create ten highly interactive, adaptive lessons and a keystone project to teach resiliency soft skills.The courseware features a playful AI character, Dot.</p>
+                  <a href="#">View case study </a>
+              </div>
+          </div>
+          <div className="firstprogram">
+              <div className="leftside">
+                  
+              </div>
+              <div className="rightside">
+                  
+                  <h5>case study</h5>
+                  <h2>Resiliency</h2> <br />
+                  <p>The Northeast Resiliency Consortium worked with Smart Sparrow to create ten highly interactive, adaptive lessons and a keystone project to teach resiliency soft skills.The courseware features a playful AI character, Dot.</p>
+                  <a href="#">View case study </a>
+              </div>
+          </div>
+          <div className="firstprogram">
+              <div className="leftside">
+                  
+              </div>
+              <div className="rightside">
+                  
+                  <h5>case study</h5>
+                  <h2>Resiliency</h2> <br />
+                  <p>The Northeast Resiliency Consortium worked with Smart Sparrow to create ten highly interactive, adaptive lessons and a keystone project to teach resiliency soft skills.The courseware features a playful AI character, Dot.</p>
+                  <a href="#">View case study </a>
+              </div>
+          </div>
+    
+      </div>
        
         </>
     )
--- src/compoenents/academics/Cardpanel/Features.js
+++ src/compoenents/academics/Cardpanel/Features.js
@@ -2,96 +2,10 @@ import React from 'react'
 import "./Features.scss"
 const Features = () => {
     return (
-        <>
-        <div className="card_section">
-  <div className="card_four">
-    
-    
-     
-    
-    <h1>Some Info</h1>
-    
-    <label>Share with team members</label>
-    <input type="text" placeholder="Enter an email or name" />
-    
-    
-    <p >some random text over here </p>
-    
-    <div className="btn">Submit </div>
-   
-  </div>
-  
-  
-  <div className="card_two">
-    
-    <h1> random Text</h1>
-    
-    <p> Anyone with the link can view this file. </p>
-    
-    
-    <div className="btn">Button</div>
-  </div>
-  
-  
-</div>
-<div className="card_section">
-  
-  
- 
- <div className="card_one">
-    
-    <h1>Random Text</h1>
-    
-    <div className="content_section">
-      <div className="section_left">
-        <h2>some information</h2>
-        <p>information elaborating the </p>
-        
-      </div>
+      <>
       
-    </div>
-    
-    <hr/>
-    <div className="content_section">
-      
-       <div className="section_left">
-        <h2 style={{marginTop:"34px"}}>Invite  collaborators</h2>
-         <p>some random text appearing here</p>
-        
-      </div>
-      <div className="section_right">
-        
-        <div className="btn">Invite</div>
-        
-    
-      </div>
-      
-      
-    </div>
-    
-  </div>
-  
-  
-  <div class="card_three">
-    
-   
-    
-    <h1>
-      Random Text 
-    </h1>
-    
-    <div class="social_icons">
-      <div class="icon">
-     <p>Lorem ipsumsdh sadfsdf asdfasd</p>
-      </div>
-    </div>
-  </div>
-  
-  
-  
-</div>
- </>  
-    )
+      </>
+     )
 }
 
 export default Features
