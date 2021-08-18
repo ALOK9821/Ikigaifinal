756ee1fa8eefd0d1d2776f3e145254754f1d2d80
Alok Yadav <alokyadav9821238248@gmail.com>
2021-08-18 20:56:29 +0530

    maybe its fine

--- src/compoenents/Home/Joinus/Joinus.js
+++ src/compoenents/Home/Joinus/Joinus.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import "./Joinus.scss"
+import Contre from "../../../assets/contcre.jpg"
+import Instrc from "../../../assets/instructor.jpg"
+import Campusa from "../../../assets/campusA.jpg"
 const Joinus = () => {
     return (
         <>
@@ -9,47 +12,24 @@ const Joinus = () => {
              <div className="joinustitle">
              <h1>Join Ikigai Team </h1> <h2> Make an impact  On the world</h2>
              </div>
-          
-          <br />
-           <div className="cards-list">
-  
-  <div className="card 1">
-    <div className="card_image"> <img src="https://images.unsplash.com/photo-1513258496099-48168024aec0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" /> </div>
-    <div className="card_title title-white">
-      <p>Content Creater</p>
-    </div>
-  </div>
-  
-    <div className="card 2">
-    <div className="card_image">
-      <img src="https://images.unsplash.com/photo-1580894732930-0babd100d356?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dGVhY2hlcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
-      </div>
-    <div className="card_title title-white">
-      <p>Instructor</p>
-    </div>
-  </div>
-  
-  <div className="card 3">
-    <div className="card_image">
-    
-      <img src="https://images.unsplash.com/photo-1524178232363-1fb2b075b655?ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8dGVhY2hlcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
-    </div>
-    <div className="card_title title-white">
-      <p>Campus Mentor</p>
-    </div>
-  </div>
-  <div className="card 3">
-    <div className="card_image">
-      <img src="https://images.unsplash.com/photo-1550096141-7263640aa48c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80" />
-    </div>
-    <div className="card_title title-white">
-      <p>Campus ambassador</p>
-    </div>
-  </div>
-    
-  
-  </div>
-               </div> 
+             <div className="jscards">
+               <div className="jscc">
+                 <div className="imgholder1">
+                   <h2>Content Creator</h2>
+                 </div>
+                 <div className="imgholder2">
+                 <h2> Instructor</h2>
+                 </div>
+                 <div className="imgholder3">
+                   <h2>Campus Mentor</h2>
+                 </div>
+                 <div className="imgholder4">
+                   <h2> Campus Ambassador</h2>
+                 </div>
+               </div>
+           </div>
+          </div>
+      
         </>
     )
 }
--- src/compoenents/Home/Testimonials/Testimonial.js
+++ src/compoenents/Home/Testimonials/Testimonial.js
@@ -11,27 +11,27 @@ const Testimonial = () => {
       <div className="testim-box">
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae iure non reiciendis reprehenderit? Dicta eaque illo ut beatae alias quod, distinctio ad, asperiores vitae culpa qui nisi rerum debitis modi.</p>
         <div className="testim-box-content">
-         <img src="https://i.postimg.cc/rpRQv1sc/3.jpg" alt="" />
-          <h3>Alok  Yadav</h3>
-          <p>FrontEnd Developer</p>
+         {/* <img src="https://i.postimg.cc/rpRQv1sc/3.jpg" alt="" /> */}
+          <h3>Person name</h3>
+          <p>Desiginition</p>
         </div>
       </div>
         <div className="testim-box">
 
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae iure non reiciendis reprehenderit? Dicta eaque illo ut beatae alias quod, distinctio ad, asperiores vitae culpa qui nisi rerum debitis modi.</p>
           <div className="testim-box-content">
-            <img src="https://i.postimg.cc/rpRQv1sc/3.jpg" alt="" />
-            <h3>Alok Yadav</h3>
-            <p>FrontEnd Developer</p>
+            {/* <img src="https://i.postimg.cc/rpRQv1sc/3.jpg" alt="" /> */}
+            <h3>Person Name</h3>
+            <p>desigination</p>
           </div>
         </div>
           <div className="testim-box">
   
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae iure non reiciendis reprehenderit? Dicta eaque illo ut beatae alias quod, distinctio ad, asperiores vitae culpa qui nisi rerum debitis modi.</p>
             <div className="testim-box-content">
-              <img src="https://i.postimg.cc/rpRQv1sc/3.jpg" alt="" />
-              <h3>alok yadav</h3>
-              <p>FrontEnd Developer</p>
+              {/* <img src="https://i.postimg.cc/rpRQv1sc/3.jpg" alt="" /> */}
+              <h3>Person Name</h3>
+              <p>Desiginition</p>
             </div>
           </div>
 
